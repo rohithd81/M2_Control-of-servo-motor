b96460f5f4c41fdabd5733199c99ce6296923a4e
rohithd81 <74349463+rohithd81@users.noreply.github.com>
2022-04-22 10:38:48 +0530

    Update and rename Project.c to project_main.c

---
+++ 3_Implementation/project_main.c
@@ -0,0 +1,21 @@
+
+   
+/**
+ * @file project_main.c
+ * @author ROHITH D (rohith.d.2018.ece@ritchennai.edu.in)
+ * @brief Header file for Control of Servo Motor are connected to AVR328 MCU GPIO pin
+ * @version 0.1
+ * @date 2022-04-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <Servo.h>
+Servo myservo;
+int pos = 0;
+
+void main()
+{
+void setup();
+void loop();
+}
